e89cf119eb2ec49f2e05060bdbedc3d283c95918
Sam Mulvey <sam@vis.nu>
2023-04-30 23:20:17 -0700

    changes to sections

--- generate/parse_sections.php
+++ generate/parse_sections.php
@@ -3,17 +3,21 @@
 $lines = explode("\n", trim(file_get_contents("sections.csv")));
 
 $sections = [];
+$json = [];
 $curr = '';
 
 foreach ($lines as $l) {
 	$b = str_getcsv($l);
-	//print_r($b);continue;
+
+	$b[1] = substr($b[1], 0, -2);
+
 	if (strlen($b[1]) == 0) {
 		$curr = $b[0];
 		$sections[$curr] = [];
 		continue;
 	} else {
 		$sections[$curr][] = $b;
+		$json[$b[0]] = $b[1];
 	}
 }
 
@@ -28,3 +32,8 @@ foreach ($sections as $area=>$s) {
 
 
 }
+
+echo "<script>\nvar sections = ";
+echo json_encode($json, JSON_PRETTY_PRINT);
+echo ";\n</script>\n";
+
